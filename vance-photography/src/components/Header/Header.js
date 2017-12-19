352e13c6dd56ae9ad5add1139c08aeef09bfcfa1
Trent Bennett <rehab.rtb@gmail.com>
2017-12-19 15:12:55 -0700

    header changes

--- vance-photography/src/components/Header/Header.js
+++ vance-photography/src/components/Header/Header.js
@@ -6,12 +6,17 @@ class Header extends Component {
   render() {
     return (
 
-      <div className="dropdown">
+      <div className="container">
+        <header>
+          <nav>
+            <h1 className="title" >Vance_<span>Photography</span></h1>
             <ul>
               <li><Link to='/'>HOME</Link></li>
               <li><Link to='/gallery'>GALLERY</Link></li>
               <li><Link to='/about'>ABOUT</Link></li>
             </ul>
+          </nav>
+        </header>
       </div>
     );
   }
