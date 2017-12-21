3570221c4763de644c2408680a5b1f7d7d4c971e
Trent Bennett <rehab.rtb@gmail.com>
2017-12-21 09:51:35 -0700

    header changes

--- vance-photography/src/components/Header/Header.js
+++ vance-photography/src/components/Header/Header.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import './Header.css';
 
 class Header extends Component {
-  render() {
-    return (
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = {
+      dropMenu: false
+    };
+  }
 
+  toggleMenu() {
+    let menuState = !this.state.menuActive;
+    this.setState({
+      dropMenu: menuState
+    });
+  }
+
+  render() {
+
+    let menu;
+    if(this.state.dropMenu) {
+      menu = <div>
+                <ul>
+                  <li>First Item </li>
+                  <li>Second Item </li>
+                  <li>Third Item </li>
+                </ul>
+              </div>
+    } else {
+      menu = "";
+    }
+
+    return (
       <div className="container">
         <header>
           <nav>
@@ -16,6 +45,12 @@ class Header extends Component {
               <li><Link to='/about'>About</Link></li>
             </ul>
           </nav>
+          <div id = "menu">
+        <i className = "fa fa-plus" onClick= { this.toggleMenu }/>
+      <CSSTransitionGroup transitionName = "menu" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+        {menu}
+      </CSSTransitionGroup>
+    </div>
         </header>
       </div>
     );
