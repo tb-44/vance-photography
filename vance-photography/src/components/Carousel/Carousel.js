99a105df1c4c75e42dd03c57362447fd25caafe4
Trent Bennett <rehab.rtb@gmail.com>
2018-08-15 18:27:47 -0600

    new changes

--- vance-photography/src/components/Carousel/Carousel.js
+++ vance-photography/src/components/Carousel/Carousel.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import ImageSlide from './ImageSlide';
 import Arrow from './Arrow';
 
+const imgUrls = [
+	"https://cmeimg-a.akamaihd.net/640/clsd/getty/c64f76dc20c246ca88ee180fe4b4b781", 
+	"https://lh3.googleusercontent.com/oxPeODS2m6rYIVbhcQChRtOWEYeGDwbeeeB1cDU2o_WYAVPU61VIgx-_6BAh5gSL8Sw=h900",
+	"https://i0.wp.com/www.universodegatos.com/wp-content/uploads/2017/04/fivfelv7.jpg?resize=582%2C328",
+	"https://i.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg",
+	"https://ehealthforum.com/health/images/avatars/11699147425707699031013.jpeg"
+];
+
 class Carousel extends Component {
   constructor(props) {
     super(props);
@@ -37,6 +45,7 @@ class Carousel extends Component {
   }
 
   render() {
+
     return (
       <div className="carousel">
         <Arrow
