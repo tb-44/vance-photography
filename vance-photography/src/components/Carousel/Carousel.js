8c8d918aaf344ebbfe23107e85f3b0a06407b63c
Trent Bennett <rehab.rtb@gmail.com>
2018-08-15 18:08:00 -0600

    carousel updates

--- vance-photography/src/components/Carousel/Carousel.js
+++ vance-photography/src/components/Carousel/Carousel.js
@@ -3,23 +3,56 @@ import ImageSlide from './ImageSlide';
 import Arrow from './Arrow';
 
 class Carousel extends Component {
-    render () {
-        return (
-          <div className="carousel">
-            <Arrow
-              direction="left"
-              clickFunction={ this.previousSlide }
-              glyph="&#9664;" />
-    
-            <ImageSlide url={ imgUrl } />
-    
-            <Arrow
-              direction="right"
-              clickFunction={ this.nextSlide }
-              glyph="&#9654;" />
-          </div>
-        );
-      }
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImageIndex: 0
+    };
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+  previousSlide() {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide() {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  render() {
+    return (
+      <div className="carousel">
+        <Arrow
+          direction="left"
+          clickFunction={this.previousSlide}
+          glyph="&#9664;" />
+
+        <ImageSlide url={imgUrl} />
+
+        <Arrow
+          direction="right"
+          clickFunction={this.nextSlide}
+          glyph="&#9654;" />
+      </div>
+    );
+  }
+}
 
 export default Carousel;
